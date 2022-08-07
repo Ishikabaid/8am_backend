a349878349f0ae2e8d16856c9ec7784eb314f0ed
Ishikabaid <ibishikabaid@gmail.com>
2022-08-07 22:35:37 +0530

    server.js update

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ app.use('/api',itemRoutes);
 app.use('/api',cartRoutes);
 app.use('/api',orderRoutes);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    });
-}
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+//     app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+//     });
+// }
 
 const dbURL = config.get('dbURL');
-const port = process.env.PORT || 4000;
+const port = (process.env.PORT || 4000);
 // mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect('mongodb+srv://cluster0.1lstfbx.mongodb.net/?retryWrites=true&w=majority', { user: 'ishika', pass: 'ishikabaid', useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
